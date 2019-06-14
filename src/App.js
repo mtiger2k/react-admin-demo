3114d37bf10a894313129e0445faa4596c9e5fdb
shaoqintian <454412129@qq.com>
2019-06-14 11:07:03 +0800

    initial import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import dataProviderFactory from './dataProvider';
+import fakeServerFactory from './fakeServer';
+
+class App extends Component {
+    state = { dataProvider: null };
+
+    async componentWillMount() {
+        this.restoreFetch = await fakeServerFactory(
+            process.env.REACT_APP_DATA_PROVIDER
+        );
+
+        const dataProvider = await dataProviderFactory(
+            process.env.REACT_APP_DATA_PROVIDER
+        );
+
+        this.setState({ dataProvider });
+    }
+
+    componentWillUnmount() {
+        this.restoreFetch();
+    }
+
+    render() {
+        const { dataProvider } = this.state;
+
+        if (!dataProvider) {
+            return (
+                <div className="loader-container">
+                    <div className="loader">Loading...</div>
+                </div>
+            );
+        }
+
+        return (
+          <Admin dataProvider={dataProvider} >
+            <Resource name="customers" list={ListGuesser} edit={EditGuesser} />
+            <Resource name="commands" list={ListGuesser} edit={EditGuesser} options={{ label: 'Orders' }} />
+            <Resource name="invoices" list={ListGuesser} edit={EditGuesser} />
+            <Resource name="products" list={ListGuesser} edit={EditGuesser} />
+            <Resource name="categories" list={ListGuesser} edit={EditGuesser} />
+            <Resource name="reviews" list={ListGuesser} edit={EditGuesser} />
+          </Admin>
+        );
+    }
 }
 
 export default App;
